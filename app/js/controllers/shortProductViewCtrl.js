6dc6c94cddd9e4c3ec9e9b6390fc9d747d06c144
Jen Rasmussen <jrasmussen@four51.com>
2016-05-09 10:35:52 -0500

    Product List Add All To Cart [ still needs modularized ]

--- app/js/controllers/shortProductViewCtrl.js
+++ app/js/controllers/shortProductViewCtrl.js
@@ -1,6 +1,13 @@
-four51.app.controller('shortProductViewCtrl', ['$routeParams', '$scope', 'ProductDisplayService', 'Order', 'User', '$location', '$route', function ($routeParams, $scope, ProductDisplayService) {
+four51.app.controller('shortProductViewCtrl', ['$routeParams', '$scope', 'ProductDisplayService', 'Order', 'User', '$location',
+	function ($routeParams, $scope, ProductDisplayService, Order, User, $location) {
 	$scope.LineItem = {};
 	$scope.LineItem.Product = $scope.p;
 	ProductDisplayService.setNewLineItemScope($scope);
 	ProductDisplayService.setProductViewScope($scope);
+
+	/*product list add all to cart*/
+	$scope.currentPath = $location.path();
+	console.log($scope.currentPath);
+	$scope.allowAddToOrderInProductList = $scope.allowAddToOrder && $scope.LineItem.Product.Type != 'VariableText' && $scope.LineItem.Product.SpecCount == 0;
+	/*product list add all to cart*/
 }]);
