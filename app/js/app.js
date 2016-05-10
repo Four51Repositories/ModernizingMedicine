62cc08268ef5388f4591e715e86901c71d194efb
Jen Rasmussen <jrasmussen@four51.com>
2016-05-09 21:08:48 -0500

    Custom Solutions [ product search, security modal, terms modal ]

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,5 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms', 'OrderCloud-HeaderNavigation', 'OrderCloud-BackToTop', 'OrderCloud-Breadcrumbs',
-    'OrderCloud-ProductListAddAllToCart']);
+    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms', 'OrderCloud-HeaderNavigation', 'OrderCloud-ProductSearchInput', 'OrderCloud-BackToTop',
+    'OrderCloud-Breadcrumbs', 'OrderCloud-ProductListAddAllToCart', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal']);
