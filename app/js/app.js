17c6911d41451271abdb161289642a7071d44833
Jen Rasmussen <jrasmussen@four51.com>
2016-05-16 08:58:33 -0500

    Custom Solutions [ hamburger nav ]

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,6 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms', 'OrderCloud-HeaderNavigation', 'OrderCloud-ProductSearchInput', 'OrderCloud-BackToTop',
-    'OrderCloud-Breadcrumbs', 'OrderCloud-MiniCart', 'OrderCloud-ProductListAddAllToCart', 'OrderCloud-ProductMatrix', 'OrderCloud-SameAsShippingCheckbox', 'OrderCloud-SecurityModal',
-    'OrderCloud-TermsModal']);
+    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms', 'OrderCloud-HeaderNavigation', 'OrderCloud-ProductSearchInput',
+    'OrderCloud-BackToTop', 'OrderCloud-Breadcrumbs', 'OrderCloud-MiniCart', 'OrderCloud-HamburgerNavigation', 'OrderCloud-ProductListAddAllToCart', 'OrderCloud-ProductMatrix',
+    'OrderCloud-SameAsShippingCheckbox', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal']);
